707e0d59e6c3fba8a0f7c08c5c4da36f22811311
Pashkevych Vitalii <ipz212_pvv@student.ztu.edu.ua>
2025-04-21 21:05:18 +0300

    done

--- api/src/routes/AdvertRoutes.js
+++ api/src/routes/AdvertRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const repository = require('../repository/advertRepository');
 const authMiddleware = require('../middlewares/authMiddleware');
 const ownerMiddleware = require("../middlewares/ownerMiddleware");
+const { getDecodedTokenFromHeader } = require("../utils/token");
+const { models } = require("../models");
 
 /**
  * @swagger
@@ -262,7 +264,29 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const advert = await repository.getAdvertById(id,{isHidden:false});
+    let where = {};
+    let user = null;
+
+    try {
+      const decoded = getDecodedTokenFromHeader(req);
+      user = await models.User.findOne({ where: { email: decoded.email } });
+    } catch (e) {
+    }
+
+    if (!user) {
+      where.isHidden = false;
+    }
+
+    const advert = await repository.getAdvertById(id, where);
+
+    if (!advert) {
+      return res.status(404).json({ error: 'Оголошення не знайдено' });
+    }
+
+    if (user && advert.userId !== user.id && advert.isHidden) {
+      return res.status(403).json({ error: 'Оголошення приховано' });
+    }
+
     res.status(200).json(advert);
   } catch (error) {
     res.status(400).json({ error: error.message });
